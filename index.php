77aa07b0903290a215295cfb524046b7753ebc64
Pritam Hate <pritam.hate@outlook.com>
2021-12-15 19:27:33 +0530

    signing section on mobile started

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php include 'header.php' ?>
 
 <div id="content-mobile">
-    <img src="./assets/images/logo-main.jpg" class="img-fluid" alt="">
+    <div class="signIn" id="sign-in">
+        <img src="./assets/images/logo-main.jpg" class="img-fluid" alt="">
+    </div>
 </div>
 <!-- Visible Only On Mobile -->
 
