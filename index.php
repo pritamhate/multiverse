8debdc969bebfcb81db8457d4f89bc9e19c5b836
Pritam Hate <pritam.hate@outlook.com>
2021-12-15 18:33:01 +0530

    mobile and desktop view ids updated

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
 <?php include 'header.php' ?>
-<h1>Hello world!</h1>
+
+<style>
+    #content-desktop {
+        display: block;
+    }
+
+    #content-mobile {
+        display: none;
+    }
+
+    @media screen and (max-width: 768px) {
+
+        #content-desktop {
+            display: none;
+        }
+
+        #content-mobile {
+            display: block;
+        }
+
+    }
+</style>
+
+<div id="content-desktop">
+    <h1>Desktop</h1>
+</div>
+
+<div id="content-mobile">
+    <h1>Mobile</h1>
+</div>
+
 <?php include 'header.php' ?>
