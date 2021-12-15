d50ef8f046eac2bb7f7f87cfa33c152922fc975b
Amol Gosavi <agosavi12@gmail.com>
2021-12-15 18:47:38 +0530

    banner-quiz-reward scss created

--- electro.php
+++ electro.php
@@ -1,5 +1,11 @@
 <?php include('header.php');?>
 
+<div id="content-desktop">
+    <h1>Desktop</h1>
+</div>
 
+<div id="content-mobile">
+    <h1>Mobile</h1>
+</div>
 
 <?php include('footer.php');?>
