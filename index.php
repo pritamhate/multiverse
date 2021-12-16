c8a3605081b2ed2b6310c6112dabfc32e4bc99c0
Pritam Hate <pritam.hate@outlook.com>
2021-12-16 13:43:54 +0530

    signup form done with jquery validate

--- index.php
+++ index.php
@@ -14,20 +14,41 @@
             <p class="divider">
                 <span>OR</span>
             </p>
-            <form>
+            <form class="needs-validation" novalidate>
                 <div class="col-12 mb-3 mobileField">
-                    <input type="tel" class="form-control" placeholder="Sign up using mobile number" required >
+                    <input type="tel" class="form-control" placeholder="Sign up using mobile number" required>
                     <button type="submit" class="btn btn-link text-white arrow-button"><i class="fas fa-arrow-right"></i></button>
                 </div>
                 <div class="col-6 mb-3">
-                    <input type="password" class="form-control" placeholder="otp" id="otp">
+                    <input type="password" class="form-control" placeholder="otp" id="otp" required>
                 </div>
-                <button type="submit" class="btn btn-light rounded-0" disabled>Submit</button><br>
+                <button type="submit" class="btn btn-light rounded-0">Submit</button><br>
                 <a href="#" class="signin-tnc">T&amp;C<sup>*</sup></a>
             </form>
         </div>
     </div>
 </div>
 <!-- Visible Only On Mobile -->
+
+<script>
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (function() {
+        'use strict';
+        window.addEventListener('load', function() {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation');
+            // Loop over them and prevent submission
+            var validation = Array.prototype.filter.call(forms, function(form) {
+                form.addEventListener('submit', function(event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    form.classList.add('was-validated');
+                }, false);
+            });
+        }, false);
+    })();
+</script>
 
 <?php include 'footer.php' ?>
